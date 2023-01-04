269ab4fb97fe4054ae8b75a593929d8eace633ee
mandla <aldnammandla@live.com>
2023-01-04 22:26:15 +0200

    added geolocation to get latitude and longitude in App.js

--- simple-weather-app/src/App.js
+++ simple-weather-app/src/App.js
@@ -1,7 +1,22 @@
-// import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from "react";
+
 
 function App() {
+
+const [lat, setLat] = useState([]);
+const [long, setLong] = useState([]);
+
+useEffect(() => {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    setLat(position.coords.latitude, position.coords.longitude);
+    setLong(position.coords.longitude);
+  });
+
+  console.log("Latitude is: ", lat)
+  console.log("Longitude is: ", long)
+}, [lat, long]);
+
   return (
     <div className="App">
       {/* <header className="App-header">
