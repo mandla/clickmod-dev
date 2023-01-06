1a0a0eb8664b82df64f5bb07dbcbc352702736c8
mandla <aldnammandla@live.com>
2023-01-06 23:28:19 +0200

    Added other fields

--- simple-weather-app/src/components/weather.js
+++ simple-weather-app/src/components/weather.js
@@ -5,7 +5,11 @@ import { Card } from 'semantic-ui-react';
 const WeatherCard = ({weatherData}) => (
     <Card>
         <Card.Content>
-            <Card.Header className="header">{weatherData.name}</Card.Header>            
+            <Card.Header className="header">City Name: { weatherData.name }</Card.Header>            
+            <p>Temparature: { weatherData.main.temp }</p>
+            <p>Sunrise: { weatherData.sys.sunrise }</p>
+            <p>Sunset: { weatherData.sys.sunset }</p>
+            <p>Description: { weatherData.weather[0].description }</p>
         </Card.Content>           
     </Card>
 )
