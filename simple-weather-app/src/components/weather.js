9b5f9fd0bae2fae5b86569dc209e24a778cda016
mandla <aldnammandla@live.com>
2023-01-06 23:41:52 +0200

    Added formatting to the fields

--- simple-weather-app/src/components/weather.js
+++ simple-weather-app/src/components/weather.js
@@ -6,13 +6,13 @@ const WeatherCard = ({weatherData}) => (
     <Card>
         <Card.Content>
             <Card.Header className="header">City Name: { weatherData.name }</Card.Header>            
-            <p>Temparature: { weatherData.main.temp }</p>
-            <p>Sunrise: { weatherData.sys.sunrise }</p>
-            <p>Sunset: { weatherData.sys.sunset }</p>
-            <p>Description: { weatherData.weather[0].description }</p>
-            <p>Humidity: { weatherData.weather[0].humidity }</p>
-            <p>Windspeed: { weatherData.sys.windspeed }</p>
-            <p>Visibility: { weatherData.sys.visibility }</p>
+            <p>Temparature: { weatherData.main.temp } &deg;C</p>
+            <p>Sunrise: { new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN') }</p>
+            <p>Sunset: { new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN') }</p>
+            <p>Description: { weatherData.weather[0].main }</p>
+            <p>Humidity: { weatherData.main.humidity } %</p>
+            <p>Windspeed: { weatherData.main.windspeed }</p>
+            <p>Visibility: { weatherData.main.visibility }</p>
         </Card.Content>           
     </Card>
 )
