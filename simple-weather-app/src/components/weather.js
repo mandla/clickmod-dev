47c253fb240c476578a50079d03ff7cc0b9168e4
mandla <aldnammandla@live.com>
2023-01-07 01:00:48 +0200

    added flexbox and removed the previous Card

--- simple-weather-app/src/components/weather.js
+++ simple-weather-app/src/components/weather.js
@@ -6,24 +6,25 @@ import moment from 'moment';
 const WeatherCard = ({weatherData}) => (
     <div className="main">
             <p className="header">{weatherData.name}</p>
-        <div>
+        <div className="flex">  
             <p className="day">Day: {moment().format('dddd')}</p>    
+            <p className="day">Date: {moment().format('LL')}</p>
         </div>    
-        <div>
+
+        <div className="flex">
             <p className="temp">Temparature: { weatherData.main.temp } &deg;C</p>
+            <p className="temp">Humidity: { weatherData.main.humidity } %</p>
         </div>
-        <Card>
-            <Card.Content>
-                <Card.Header className="header">City Name: { weatherData.name }</Card.Header>            
-                <p>Sunrise: { new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN') }</p>
-                <p>Sunset: { new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN') }</p>
-                <p>Description: { weatherData.weather[0].main }</p>
-                <p>Humidity: { weatherData.main.humidity } %</p>
-                <p>Windspeed: { weatherData.main.windspeed }</p>
-                <p>Visibility: { weatherData.main.visibility }</p>
-                <p>Date: {moment().format('LL')}</p>
-            </Card.Content>           
-        </Card>
+        {/* // <Card>
+        //     <Card.Content>
+        //         <Card.Header className="header">City Name: { weatherData.name }</Card.Header>            
+        //         <p>Sunrise: { new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN') }</p>
+        //         <p>Sunset: { new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN') }</p>
+        //         <p>Description: { weatherData.weather[0].main }</p>
+        //         <p>Windspeed: { weatherData.main.windspeed }</p>
+        //         <p>Visibility: { weatherData.main.visibility }</p>
+        //     </Card.Content>           
+        // </Card> */}
     </div>
 )
 
