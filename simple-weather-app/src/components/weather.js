a0f0d78f6c5ecc1626a546dd45c0931fdb80a3b1
mandla <aldnammandla@live.com>
2023-01-07 01:11:57 +0200

    added styling and added fields inside divs

--- simple-weather-app/src/components/weather.js
+++ simple-weather-app/src/components/weather.js
@@ -15,11 +15,14 @@ const WeatherCard = ({weatherData}) => (
             <p className="temp">Temparature: { weatherData.main.temp } &deg;C</p>
             <p className="temp">Humidity: { weatherData.main.humidity } %</p>
         </div>
+        <div className="flex">
+            <p className="sunrise-sunset">Sunrise: { new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN') }</p>
+            <p className="sunrise-sunset">Sunset: { new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN') }</p>
+        </div>
         {/* // <Card>
         //     <Card.Content>
         //         <Card.Header className="header">City Name: { weatherData.name }</Card.Header>            
-        //         <p>Sunrise: { new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN') }</p>
-        //         <p>Sunset: { new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN') }</p>
+        //  
         //         <p>Description: { weatherData.weather[0].main }</p>
         //         <p>Windspeed: { weatherData.main.windspeed }</p>
         //         <p>Visibility: { weatherData.main.visibility }</p>
