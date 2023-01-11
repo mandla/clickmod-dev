686641145c194bff18baecc78446ecbad1f16052
mandla <aldnammandla@live.com>
2023-01-11 04:57:30 +0200

    removed keys from api

--- simple-weather-app/src/api.js
+++ simple-weather-app/src/api.js
@@ -1,11 +1,11 @@
 export const geoApiOptions = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "4d6025603emshf1897f4c526cdb3p1dd0f2jsn2927c340930e",
+      "X-RapidAPI-Key": "",
       "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
     },
   };
   export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
   
   export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-  export const WEATHER_API_KEY = "b49c7e3e06eaa12a063660a08bbcb8c2"; 
+  export const WEATHER_API_KEY = ""; 
